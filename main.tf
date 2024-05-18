55d4fae6090353bf16f1c42b82363d2f4eea419c
shekarg-cougarclan <164062144+shekarg-cougarclan@users.noreply.github.com>
2024-05-18 18:34:36 -0400

    Removed old SG2

--- main.tf
+++ main.tf
@@ -30,6 +30,21 @@ resource "aws_instance" "blog" {
 }
 
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+  name  = "blog"
+
+    vpc_id  =  data.aws_vpc.default.id
+
+
+    ingress_rules        = ["http-80-tcp" , "https-443-tcp"]
+    ingress_cidr_blocks  =  ["0.0.0.0/0"]
+
+    egress_rules        = ["all-all"]
+    egress_cidr_blocks  =  ["0.0.0.0/0"]        
+}
+
 resource "aws_security_group_rule" "blog_http_in" {
   type       = "ingress"
   from_port  = 80
@@ -59,20 +74,4 @@ resource "aws_security_group_rule" "blog_everything_out" {
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id  = aws_security_group.blog.id
-}
-
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
-  name  = "blog"
-
-    vpc_id  =  data.aws_vpc.default.id
-
-
-    ingress_rules        = ["http-80-tcp" , "https-443-tcp"]
-    ingress_cidr_blocks  =  ["0.0.0.0/0"]
-
-    egress_rules        = ["all-all"]
-    egress_cidr_blocks  =  ["0.0.0.0/0"]        
 }
