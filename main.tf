afd4ff85b87d33214fcfb2a22404381c041d3fbe
shekarg-cougarclan <164062144+shekarg-cougarclan@users.noreply.github.com>
2024-05-18 19:38:06 -0400

    removed https

--- main.tf
+++ main.tf
@@ -54,6 +54,13 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups  = [module.blog_sg.security_group_id]
 
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+    }
+  }
+
   target_groups = {
     ex-instance = {
       name_prefix      = "blog-"
@@ -66,14 +73,6 @@ module "alb" {
       }
     }
   }
-
-  http_tcp_listeners = [
-    {
-      port     = 80
-      protocol = "HTTP"
-      target_group_index = 0
-    }
-  ]
 
   tags = {
     Environment = "dev"
