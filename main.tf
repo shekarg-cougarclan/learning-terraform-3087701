2616b38b8ee501aab9fde5d4945c801111cee81a
shekarg-cougarclan <164062144+shekarg-cougarclan@users.noreply.github.com>
2024-05-18 22:31:56 -0400

    recreated alb

--- main.tf
+++ main.tf
@@ -46,6 +46,50 @@ resource "aws_instance" "blog" {
 }
 
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "my-alb"
+  vpc_id  = module.blog.vpc.vpc_id
+  subnet_id = module.blog.vpc.subnets[0]
+  vpc_security_group_ids  = [module.blog_sg.security_group_id]
+
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "Example"
+  }
+}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
