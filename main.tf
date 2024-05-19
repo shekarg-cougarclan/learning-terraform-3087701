e69b910ae3f66b4e0b97ce5c24db9240205c7e6c
shekarg-cougarclan <164062144+shekarg-cougarclan@users.noreply.github.com>
2024-05-18 21:46:24 -0400

    syntax fix

--- main.tf
+++ main.tf
@@ -54,25 +54,26 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups  = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http-https-redirect = {
+  listeners = [
+    {
       port     = 80
       protocol = "HTTP"
+      target_group_index = 0
     }
-  }
+  ]
 
-  target_groups = {
+  target_groups = [
     ex-instance = {
       name_prefix      = "blog-"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
+      my_target = {
         target_id = aws_instance.blog.id
         port  = 80
       }
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
