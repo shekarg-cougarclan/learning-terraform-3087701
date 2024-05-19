21e3ed8f06cd290872ec385e21acc966d85b3dc1
shekarg-cougarclan <164062144+shekarg-cougarclan@users.noreply.github.com>
2024-05-18 22:39:31 -0400

    alb fix2

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "my-alb"
-  vpc_id  = module.blog.vpc.vpc_id
+  vpc_id  = module.blog_vpc.vpc_id
   subnets  = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
